304b0a271f4faeb645d075c7fef47c9eb6319915
xienanjiang <672088678@qq.com>
2022-06-20 09:29:23 +0800

    初始化仓库

---
+++ django_test/django_xnj/views.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : views.py
+@Time    : 2022/6/18 10:27 上午
+@Author  : nanjiang.xie
+@Email   : xie672088678@163.com
+@Software: PyCharm
+"""
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        return HttpResponse('This is first test')
+    else:
+        return render(request, 'login.html')
