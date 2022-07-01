267c93ac60e673428a2f25805e9fee44feb42f31
xienanjiang <672088678@qq.com>
2022-07-01 15:36:53 +0800

    同步更新account 的student信息 no、name、phone

--- django_test/studentno.py
+++ django_test/studentno.py
@@ -16,18 +16,18 @@ import random
 
 
 def student_no_str():
-    return 10120230
+    return 1012025
 
 
 '''生成随机4位数字字符串'''
 
 
-def random_4_num_str():
-    return random.randint(1000, 9999)
+def random_5_num_str():
+    return random.randint(10000, 99999)
 
 
 def student_no():
-    return str(student_no_str()) + str(random_4_num_str())
+    return str(student_no_str()) + str(random_5_num_str())
 
 
 def get_student_name():
@@ -81,6 +81,7 @@ def update_student_phone(student_phone, i):
     cur.close()
     conn.close()
 
+
 def insert_account_data():
     conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='admin123', db='test', charset='utf8mb4')
     cur = conn.cursor()
@@ -105,8 +106,8 @@ def insert_account_data():
 def update_account_data():
     conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='admin123', db='test', charset='utf8mb4')
     cur = conn.cursor()
-    sql = "update account_data set id_card = %s where student_id = %s"
-    cur.execute("select id,mask_id_no from student_data")
+    sql = "update account_data set students_no = %s where student_id = %s"
+    cur.execute("select id,student_no from student_data")
     i = cur.fetchall()
     for r in i:
         print(r[1],r[0])
@@ -119,9 +120,9 @@ def update_account_data():
 if __name__ == '__main__':
     '''for循环生成不同的student_no、student_name'''
     # for i in range(473, 10000):
-    #     i = i + 1
+        # i = i + 1
     #     update_student_phone(get_student_phone(), i)
-    # update_student_no(student_no(),i)
-    # update_student_name(get_student_name(),i)
+    #     update_student_no(student_no(),i)
+    #     update_student_name(get_student_name(),i)
     # print('done')
     update_account_data()
